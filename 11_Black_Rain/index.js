0a38b019e3af786519e09830228e3e6582887422
sho373 <polanditblog@gmail.com>
2021-08-08 20:52:55 +0200

    Update and rename 11_Red_Rain/index.js to 11_Black_Rain/index.js

--- 11_Black_Rain/index.js
+++ 11_Black_Rain/index.js
@@ -3,14 +3,16 @@ import {Rain} from "./rain.js";
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 500;
+canvas.width = window.innerWidth - 30;
+canvas.height = window.innerHeight - 30;
+const GAME_WIDTH = canvas.width
+const GAME_HEIGHT = canvas.height
 
 let backImg = document.getElementById("img_back");
 
 let rain = [];
 
-for(var i = 0; i < 140; i++){
+for(var i = 0; i < canvas.width / 20; i++){
     rain.push(new Rain(GAME_WIDTH,GAME_HEIGHT));
 }
 
